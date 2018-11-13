619881bbead5c34450d5483f5e3fd8e985fc6087
James Smith <jsmith4925@gmail.com>
2018-11-13 18:32:13 +0000

    bug fixing

--- concepts/mapping.py
+++ concepts/mapping.py
@@ -11,29 +11,37 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 PURPLE = (128, 0, 128)
-LRED = (255, 128, 128)
 
 pygame.init()
 running = True
 
-# Smallest space: 5.
-sectorArea = 15
+# Set grid square area here: Smallest space: 5.
+sectorArea = 50
 # Resolution of the grid
-sectorYCount = 40
-sectorXCount = 40
+sectorYCount = 10
+sectorXCount = 10
 
 sizeX = 50+sectorYCount*sectorArea
-sizeZ = 50+sectorXCount*sectorArea
+sizeY = 50+sectorXCount*sectorArea
 
-# Set size.
-size = (sizeX, sizeZ)
+# Calculations to et size.
+size = (sizeX, sizeY)
 screen = pygame.display.set_mode(size)
 
 clock = pygame.time.Clock()
 screen.fill(WHITE)
-pygame.draw.rect(screen, BLACK, [25, 25, sizeX-49, sizeZ-49], 2)
+pygame.draw.rect(screen, BLACK, [25, 25, sizeX-49, sizeY-49], 2)
 
 pygame.display.flip()
+
+#Draw the grid.
+for x in range(sectorYCount):
+    pygame.draw.line(screen,BLACK,[(x*sectorArea+25),25],[(x*sectorArea+25),sizeY-25],2)        #Vertical lines.
+for x in range(sectorXCount):
+    pygame.draw.line(screen,BLACK,[25,(x*sectorArea+25)],[sizeX-25,(x*sectorArea+25)],2)        #Horizontal lines.
+
+pygame.display.flip()
+
 
 
 while running:
