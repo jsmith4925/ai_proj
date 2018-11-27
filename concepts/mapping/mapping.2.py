bfb55e9be30ccf374b72e0a0beda31601f71a8b6
James Smith <jsmith4925@gmail.com>
2018-11-27 23:33:24 +0000

    All fixed

--- concepts/mapping/mapping.2.py
+++ concepts/mapping/mapping.2.py
@@ -4,7 +4,7 @@ import random
 
 import pygame
 from pygame.locals import *
-
+import numpy as np
 #   Setting Constants
 # Set colors.
 BLACK = (0, 0, 0)
@@ -15,7 +15,8 @@ GRASS_GREEN = (51, 204, 51)
 BLUE = (0, 0, 255)
 PURPLE = (128, 0, 128)
 LIGHT_RED = (255, 128, 128)
-ROAD_BROWN = (83, 64, 45)
+ROAD_BROWN = (102, 70, 70)
+AUBURN = (165,42,42)
 
 # Set block types
 UNSET = 0
@@ -32,9 +33,9 @@ WEST = 3
 # Set grid square area here: Smallest SECTOR_AREA: 5.
 SECTOR_AREA = 45
 MARGIN = 50
-# Resolution of the grid
-SECT_X_COUNT = 5
-SECT_Y_COUNT = 10
+# Resolution of the grid (Minimum 3)
+SECT_X_COUNT = 8
+SECT_Y_COUNT = 6
 
 pygame.init()
 running = True
@@ -46,49 +47,50 @@ size = (sizeX, sizeY)
 screen = pygame.display.set_mode(size)
 grid = []
 
+# #Format to int count starting at 0
+# SECT_X_COUNT -= 1
+# SECT_Y_COUNT -= 1
 
 def new_start():
-    edge_rows = [[0,random.randint(0, SECT_Y_COUNT-1)],
-                 [SECT_X_COUNT-1, random.randint(0, SECT_Y_COUNT-1)],
-                 [random.randint(0, SECT_X_COUNT-1), 0],
-                 [random.randint(0, SECT_X_COUNT-1), SECT_Y_COUNT-1]
+    edge_rows = [[random.randint(0, SECT_Y_COUNT-1),0],
+                 [random.randint(0, SECT_Y_COUNT-1),SECT_X_COUNT-1],
+                 [0, random.randint(0, SECT_X_COUNT-1)],
+                 [SECT_Y_COUNT-1, random.randint(0, SECT_X_COUNT-1)]
                  ]
     return edge_rows[random.randint(0, 3)]
 
 
-def get_adjacent_values(full_grid, current_X, current_Y):
+def get_adjacent_values(full_grid, current_Y, current_X):
     values = [XX, XX, XX, XX]
     # current position = full_grid[current_X][current_Y]]
     # with out of bounds check
     # North
-    print(full_grid)
     if current_Y < SECT_Y_COUNT-1:
-        values[0] = full_grid[current_X][current_Y+1]
+        values[2] = full_grid[current_Y+1][current_X]
     # East
     if current_X < SECT_X_COUNT-1:
-        values[1] = full_grid[current_X+1][current_Y]
+        values[1] = full_grid[current_Y][current_X+1]
     # South
     if current_Y > 0:
-        values[2] = full_grid[current_X][current_Y-1]
+        values[0] = full_grid[current_Y-1][current_X]
     # West
     if current_X > 0:
-        values[3] = full_grid[current_X-1][current_Y]
+        values[3] = full_grid[current_Y][current_X-1]
     # returns a 4 digit array - [N, E, S, W]
     return values
 
 
 def new_maze():
     # make a new empty map
-    grid = [[0] * SECT_X_COUNT for i in range(SECT_Y_COUNT)]
+    grid = [[0] * ((SECT_X_COUNT)) for i in range((SECT_Y_COUNT))]
     # Set surrounding wall
-    for row in range(SECT_X_COUNT-1):
+    for column in range(SECT_X_COUNT):
+        grid[0][column] = WALL
+        grid[SECT_Y_COUNT-1][column] = WALL
+
+    for row in range(SECT_Y_COUNT):
         grid[row][0] = WALL
         grid[row][SECT_X_COUNT-1] = WALL
-        #FIX MEEEEEE
-    for column in range(SECT_Y_COUNT):
-        grid[0][column] = WALL
-        print(grid)
-        grid[SECT_Y_COUNT-1][column] = WALL
 
     # Set Start Location
     start = new_start()
@@ -108,7 +110,7 @@ def new_maze():
     grid[finish[0]][finish[1]] = GOAL
     # The entrance to the finish always needs to be clear
     # we set this as road by default here
-    print("Start: " + str(start[0]))
+    print("Start: " + str(start))
     print("Finish: " + str(finish))
     # Plan Route
     grid_complete = plan_route(grid, start, finish)
@@ -128,17 +130,17 @@ def draw_maze(grid):
     # Fill the grid
     for x in range(SECT_X_COUNT):
         for y in range(SECT_Y_COUNT):
-            if grid[x][y] == UNSET:
+            if grid[y][x] == UNSET:
                 pygame.draw.rect(screen, GRASS_GREEN, [
                                  26+(x*SECTOR_AREA), 26+(y*SECTOR_AREA), SECTOR_AREA, SECTOR_AREA], 0)
-            elif grid[x][y] == ROUTE:
+            elif grid[y][x] == ROUTE:
                 pygame.draw.rect(screen, ROAD_BROWN, [
                                  26+(x*SECTOR_AREA), 26+(y*SECTOR_AREA), SECTOR_AREA, SECTOR_AREA], 0)
-            elif grid[x][y] == WALL:
+            elif grid[y][x] == WALL:
                 pygame.draw.rect(screen, GREEN, [
                                  26+(x*SECTOR_AREA), 26+(y*SECTOR_AREA), SECTOR_AREA, SECTOR_AREA], 0)
-            elif grid[x][y] == GOAL:
-                pygame.draw.rect(screen, RED, [
+            elif grid[y][x] == GOAL:
+                pygame.draw.rect(screen, AUBURN, [
                                  26+(x*SECTOR_AREA), 26+(y*SECTOR_AREA), SECTOR_AREA, SECTOR_AREA], 0)
     return grid
 
